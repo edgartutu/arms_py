ffc0fd7422935caa109d9c8222f061da9a534ec3
Alvin Abaho <alvinabaho@gmail.com>
2019-02-25 16:35:16 +0300

    [clean-up] Adds pep8 styling and deletes redundant function

--- fyp.py
+++ fyp.py
@@ -1,27 +1,9 @@
 p=[]
-def even_or_odd(a=min,b=max,c=str):
-    for i in range (a,b+1):
-        if(c=='even' and  i%2==0):
+def even_or_odd(a=min, b=max, c=str):
+    for i in range (a, b+1):
+        if(c=='even' and  i%2 == 0):
             p.append(i)
-        elif(c=='odd' and i%2!=0):
+        elif(c=='odd' and i%2 != 0):
             p.append(i)    
 even_or_odd(2,10,'even')
 print(len(p))
-
-p=[]
-def odd_even(a,b,category=str):
-    for i in range (a-1,b):
-        i=i+1
-        if (category=='even' and i%2==0):
-            p.append(i)
-
-        elif(category == 'odd' and i%2!=0):
-            p.append(i)
-        
-odd_even(2,10,'even')
-print(len(p))
-
-    
-        
- 
-    
