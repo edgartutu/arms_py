09eca1dfac64418947596b89fee542c3e51d7157
edgartutu <46530248+edgartutu@users.noreply.github.com>
2019-02-25 10:17:37 +0300

    update of fyp file
    remove spaces

--- fyp.py
+++ fyp.py
@@ -3,11 +3,8 @@ def even_or_odd(a=min,b=max,c=str):
     for i in range (a,b):
         if(c=='even' and  i%2==0):
             p.append(i)
-            
         elif(c=='odd' and i%2!=0):
-            p.append(i)
-            
-        
+            p.append(i)    
 even_or_odd(2,10,'odd')
 print(len(p))
 
