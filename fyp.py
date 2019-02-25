5a86201d13a4a36a5d90628cf8f0ed9dc8571174
edgartutu <walugembe.john@gmail.com>
2019-02-25 13:05:07 +0300

    fyp assignment one

---
+++ fyp.py
@@ -0,0 +1,20 @@
+p=[]
+def even_or_odd(a=min,b=max,c=str):
+    for i in range (a,b):
+        if(c=='even' and  i%2==0):
+            p.append(i)
+            
+        elif(c=='odd' and i%2!=0):
+            p.append(i)
+            
+        
+even_or_odd(2,10,'odd')
+print(len(p))
+
+            
+        
+        
+    
+        
+ 
+    
