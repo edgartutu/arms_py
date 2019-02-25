73bc45df30986a6c8ad8bb2156dc08662ee6a26b
Alvin Abaho <alvinabaho@gmail.com>
2019-02-25 17:12:02 +0300

    [Lisa & John] Adds assignment for more efficient function

--- fyp.py
+++ fyp.py
@@ -1,11 +1,14 @@
 def new_even_or_odd(minimum, maximum, category):
-    odd_list = [number for number in range(minimum, maximum + 1) if number % 2 != 0]
-    even_list = [number for number in range(minimum, maximum + 1) if number % 2 == 0]
     if category == 'even':
-        return len(even_list)
+        return len([number for number in range(minimum, maximum + 1) if number % 2 == 0])
     elif category == 'odd':
-        return len(odd_list)
-    else:
-        return None
+        return len([number for number in range(minimum, maximum + 1) if number % 2 == 0])
+    return None
 
 print(new_even_or_odd(2, 10, 'even'))
+
+# todo: Work this out
+# even_counter = 0
+# odd_counter = 0 
+# loop while checking if num is even -> increment even_counter
+# else loop while checking if num is odd -> increment odd_counter
