2fb5254c0ee73c90f5c58abe556bf152780dd9ce
Alvin Abaho <alvinabaho@gmail.com>
2019-02-25 16:54:15 +0300

    [John & Lisa] Removes deprecated function and adds new even odd counter

--- fyp.py
+++ fyp.py
@@ -1,9 +1,11 @@
-p=[]
-def even_or_odd(a=min, b=max, c=str):
-    for i in range (a, b+1):
-        if(c=='even' and  i%2 == 0):
-            p.append(i)
-        elif(c=='odd' and i%2 != 0):
-            p.append(i)    
-even_or_odd(2,10,'even')
-print(len(p))
+def new_even_or_odd(minimum, maximum, category):
+    odd_list = [number for number in range(minimum, maximum + 1) if number % 2 != 0]
+    even_list = [number for number in range(minimum, maximum + 1) if number % 2 == 0]
+    if category == 'even':
+        return len(even_list)
+    elif category == 'odd':
+        return len(odd_list)
+    else:
+        return None
+
+print(new_even_or_odd(2, 10, 'even'))
